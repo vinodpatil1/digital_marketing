6a698ecf5497bdd7669312c2f7cdc48fabae625f
DESKTOP-P7P3MBD\Admin <vinod.patil377@gmail.com>
2022-08-14 10:14:23 +0530

    Updates

---
+++ precache-manifest.68d4e3259b2a1417ec70c9ff6ace6fb1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a72d09cc526f8786e94",
+    "url": "/digital_marketing/static/css/main.b628eb0d.chunk.css"
+  },
+  {
+    "revision": "4a72d09cc526f8786e94",
+    "url": "/digital_marketing/static/js/main.4a72d09c.chunk.js"
+  },
+  {
+    "revision": "e4340b602ac115eacaff",
+    "url": "/digital_marketing/static/css/1.16f33985.chunk.css"
+  },
+  {
+    "revision": "e4340b602ac115eacaff",
+    "url": "/digital_marketing/static/js/1.e4340b60.chunk.js"
+  },
+  {
+    "revision": "64c46cf43503927088c2",
+    "url": "/digital_marketing/static/js/runtime~main.64c46cf4.js"
+  },
+  {
+    "revision": "64fdabf49ee5f24bd88ccbb56e58f9cb",
+    "url": "/digital_marketing/static/media/banner1.64fdabf4.png"
+  },
+  {
+    "revision": "5bfa61c67708c21045e44bb746656686",
+    "url": "/digital_marketing/static/media/banner2.5bfa61c6.png"
+  },
+  {
+    "revision": "5c4d825ecbbe41e0aefa2106e2703d6a",
+    "url": "/digital_marketing/static/media/banner3.5c4d825e.png"
+  },
+  {
+    "revision": "d4a90b58e0150aaa6382eda5a49f845f",
+    "url": "/digital_marketing/static/media/banner4.d4a90b58.png"
+  },
+  {
+    "revision": "5d407e3eb00391b5359f10c13b53f7a4",
+    "url": "/digital_marketing/static/media/bussiness_img.5d407e3e.png"
+  },
+  {
+    "revision": "1fbc30ca7001c3256d043675304e7adc",
+    "url": "/digital_marketing/static/media/projectP.1fbc30ca.png"
+  },
+  {
+    "revision": "dce6c2c3069fcfdfdedde2cf5936c743",
+    "url": "/digital_marketing/static/media/work1.dce6c2c3.png"
+  },
+  {
+    "revision": "9210fbb75626ace2f32cceb80c357a0f",
+    "url": "/digital_marketing/static/media/work2.9210fbb7.png"
+  },
+  {
+    "revision": "8d1c00bc06c8870c60959652d1a96860",
+    "url": "/digital_marketing/static/media/work3.8d1c00bc.png"
+  },
+  {
+    "revision": "523453d9f5e73a59023a3f7c5bad6856",
+    "url": "/digital_marketing/static/media/work4.523453d9.png"
+  },
+  {
+    "revision": "a28234f719fdb568b5ed07325d728e49",
+    "url": "/digital_marketing/static/media/about.a28234f7.png"
+  },
+  {
+    "revision": "e2293aa3d751c1de46f612b57e626fbd",
+    "url": "/digital_marketing/static/media/wp1.e2293aa3.png"
+  },
+  {
+    "revision": "990e4e6b8f23ca59e7a0f5ad780ca39d",
+    "url": "/digital_marketing/static/media/wp2.990e4e6b.png"
+  },
+  {
+    "revision": "3337345fde8d3bd583f4395e3f489de0",
+    "url": "/digital_marketing/static/media/wp3.3337345f.png"
+  },
+  {
+    "revision": "a522116b00d89c89d92be9ea3c082bac",
+    "url": "/digital_marketing/static/media/wp4.a522116b.png"
+  },
+  {
+    "revision": "ca24dadb8d29d1f698329442d220cae5",
+    "url": "/digital_marketing/static/media/contactus.ca24dadb.jpg"
+  },
+  {
+    "revision": "bf4a0b013bd00db073c77ca8ccfb0673",
+    "url": "/digital_marketing/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/digital_marketing/precache-manifest.0ef9744373fde308cf194de7cd49b544.js"
+  "/digital_marketing/precache-manifest.68d4e3259b2a1417ec70c9ff6ace6fb1.js"
 );
 
 workbox.clientsClaim();
